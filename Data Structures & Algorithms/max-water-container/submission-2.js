f7e09069f6656325ab70f8e3652e0c1a6ad6026e
Tilak Thapa <tilakthapa9898@gmail.com>
2026-06-08 18:54:47 +0545

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let maxArea = 0;
+
+        let start = 0;
+        let end = heights.length - 1;
+        while (start < end) {
+            const area = (end-start) * Math.min(heights[start], heights[end])
+            if (area > maxArea) {
+                maxArea = area;
+            } else {
+                if (heights[start] > heights[end]) {
+                    end--;
+                } else {
+                    start++;
+                }
+            }
+
+        }
+
+        return maxArea;
+    }
+
+    area(space, heights) {
+        return ;
+    }
+}
